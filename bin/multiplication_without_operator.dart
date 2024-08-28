223bfd8d057c98534093c446dcfe8f7570b2691e
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-28 08:24:18 +0300

    create multiplication without operator

---
+++ bin/multiplication_without_operator.dart
@@ -0,0 +1,13 @@
+
+void main(){
+  //5*4 = 5+5+5+5
+  //n = number
+  //m = steps
+  int n=5;
+  int m=4;
+  int result =0;
+  for(int i=1; i<=m;i++){
+    result+=n;
+  }
+  print (result);
+}
---
+++ test/multiplication_without_operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:multiplication_without_operator/multiplication_without_operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
